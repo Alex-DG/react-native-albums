a809647a3c6970282403955fe1ecc9368642d78a
Alexandre Di Guida <alex@gearedapp.co.uk>
2017-10-14 11:43:24 +0100

    Change eslint rules to airbnb

--- App.js
+++ App.js
@@ -1,15 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
 
 const instructions = Platform.select({
@@ -18,27 +12,6 @@ const instructions = Platform.select({
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
-
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text>
-          First React Native App!
-        </Text>
-        <Text>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -59,3 +32,22 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+const App = () => (
+  <View style={styles.container}>
+    <Text style={styles.welcome}>
+      Welcome to React Native!
+    </Text>
+    <Text>
+      First React Native App!
+    </Text>
+    <Text>
+      To get started, edit App.js
+    </Text>
+    <Text style={styles.instructions}>
+      {instructions}
+    </Text>
+  </View>
+);
+
+export default App;
