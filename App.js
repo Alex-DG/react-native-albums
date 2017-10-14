dfdb072ef487da38f6d8f47dafe8948dd0d1a7a6
Alexandre Di Guida <alex@gearedapp.co.uk>
2017-10-14 12:14:50 +0100

    Add Header component

--- App.js
+++ App.js
@@ -1,52 +1,11 @@
 import React from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { View } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginTop: 10,
-    marginBottom: 5,
-  },
-});
+import Header from './src/components/Header';
 
 const App = () => (
-  <View style={styles.container}>
-    <Text style={styles.welcome}>
-      Welcome to React Native!
-    </Text>
-    <Text>
-      First React Native App!
-    </Text>
-    <Text>
-      To get started, edit App.js
-    </Text>
-    <Text style={styles.instructions}>
-      {instructions}
-    </Text>
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
   </View>
 );
 
--- README.md
+++ README.md
@@ -2,3 +2,31 @@
 - estlint / airbnb config
 - jest
 - one entry point App.js for both platforms
+
+### Setup
+
+#### React Native CLI
+
+```
+$ npm install -g react-native-cli
+```
+
+#### Dependencies
+
+```
+$ npm install
+```
+
+### Commands
+
+#### Run iOS
+
+```
+$ react-native run-ios
+```
+
+#### Run Android
+
+```
+$ react-native run-ios
+```
