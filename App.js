b21bafde5be7d4dc155ee7b5937ea44a37b89f9f
Alexandre Di Guida <alex@gearedapp.co.uk>
2017-10-28 13:33:22 +0100

    Add axios API call - Add component Album list

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
 
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
   <View style={{ flex: 1 }}>
-    <Header headerText={'Albums'} />
+    <Header headerText={'TopHeader'} />
+    <AlbumList />
   </View>
 );
 
